7b24211fea588968dfd450f314f417c7fbe68cdd
github username <github email address>
2018-08-09 13:18:24 -0400

    This was good practice.

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
+require 'pry'
 require './environment'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ships = params[:pirate][:ships].map {|ship| Ship.new(ship)}
+      erb :"pirates/show"
+    end
 
   end
 end
